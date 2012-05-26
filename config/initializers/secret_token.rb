2c209a78744a65db1b6149538e0be6bb7f7098d8
Mark Guk <mark.guk.e@gmail.com>
2012-05-27 01:33:05 +0300

    Initial rails empty app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HomeBalance::Application.config.secret_token = '04b5061a09645d19cfd8f8832bd37a8364d7d1e0250be6a611715dd710db56f9b86eed4bec09fded402bc0bee5bf5b9410ecd3b431718de7855272dcf5220631'
